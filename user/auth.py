fecae320d10f12793f7ebe12b9df445bd7e2c525
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-11-27 01:29:46 +0200

    Added auth.py & schemas for User model.

---
+++ user/auth.py
@@ -0,0 +1,31 @@
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy
+
+from user.manager import get_user_manager
+from user.models import User
+
+cookie_transport = CookieTransport(cookie_max_age=3600)
+
+SECRET = "SECRET"
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_user = fastapi_users.current_user()
+current_active_user = fastapi_users.current_user(active=True)
+current_active_verified_user = fastapi_users.current_user(active=True, verified=True)
+current_superuser = fastapi_users.current_user(active=True, superuser=True)
