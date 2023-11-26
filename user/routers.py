3eae2a3a2d0af16daf71004fc869a5719e13b823
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-11-27 01:30:17 +0200

    Added utils.py & routers for User model.

---
+++ user/routers.py
@@ -0,0 +1,38 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from dependencies import get_db
+from user.auth import auth_backend, fastapi_users
+from user.models import User
+from user.schemas import UserRead, UserCreate, UserUpdate
+
+router = APIRouter()
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@router.get("/users", tags=["users"])
+async def read_users(db: AsyncSession = Depends(get_db)):
+    query = select(User)
+    users = await db.execute(query)
+    users = [user for user in users.scalars()]
+    return users
